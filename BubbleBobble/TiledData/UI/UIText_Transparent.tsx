49c04a497791aade79e48f4ceadc33768ad34613
Xeiv360 <145395703+XeivUPC@users.noreply.github.com>
2024-03-08 20:25:07 +0100

    UpdateSprites

---
+++ BubbleBobble/TiledData/UI/UIText_Transparent.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="UIText_Transparent" tilewidth="8" tileheight="8" tilecount="512" columns="64">
+ <image source="../../Assets/Sprites/TextTransparent.png" width="512" height="64"/>
+</tileset>
